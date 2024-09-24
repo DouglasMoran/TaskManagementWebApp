2e50dae1dafd7aedea6836f4850dbf9c7ebfd237
DouglasMoran <38759697+DouglasMoran@users.noreply.github.com>
2024-09-24 09:24:27 -0600

    feat: :art: adding eslint, prettier and husky config

--- eslint.config.js
+++ eslint.config.js
@@ -1,21 +1,28 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import typescriptEslint from '@typescript-eslint/eslint-plugin';
+import typescriptParser from '@typescript-eslint/parser';
+import prettier from 'eslint-plugin-prettier';
 
-export default tseslint.config(
-  { ignores: ['dist'] },
+// Configuración de ESLint en formato plano
+const config = [
+  // Reglas recomendadas de ESLint
+  js.configs.recommended,
+  // Reglas recomendadas de TypeScript
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
+    files: ['**/*.{ts,tsx,js,jsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      parser: typescriptParser, // Usar el parser de TypeScript
     },
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      '@typescript-eslint': typescriptEslint,
+      prettier,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
@@ -23,6 +30,18 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
+      'prettier/prettier': 'error',
+      '@typescript-eslint/no-unused-vars': ['warn'], // Ejemplo de regla
+      '@typescript-eslint/explicit-module-boundary-types': 'off', // Ajusta según tus necesidades
     },
   },
-)
+  // Configuración de Prettier
+  {
+    plugins: { prettier },
+    rules: {
+      'prettier/prettier': 'error',
+    },
+  },
+];
+
+export default config;
