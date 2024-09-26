ce5974d8c69f469c5aab1db84e5866414887338d
DouglasMoran <38759697+DouglasMoran@users.noreply.github.com>
2024-09-25 18:57:49 -0600

    build(dependencies): adding main dependencies for project structure

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { StrictMode } from 'react';
+
 import App from './App.tsx';
+
 import './styles/globals.css';
 import './index.css';
 
